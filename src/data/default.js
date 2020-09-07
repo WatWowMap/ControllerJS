fac2d605ea78e88b32d5000252c11fbf1fb271f8
versx <versx@ver.sx>
2020-09-07 11:13:11 -0700

    Remove settings page for now. Add start time.

--- src/data/default.js
+++ src/data/default.js
@@ -2,6 +2,7 @@
 
 const config = require('../config.json');
 const data = require('../../static/locales/' + config.locale + '.json');
+data.started = new Date().toLocaleString();
 data.title = config.title;
 data.locale = config.locale;
 data.locale_new = config.locale;
