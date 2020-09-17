61242fed3eaaafb94289c00204dba5c4b18cde08
versx <versx@ver.sx>
2020-09-17 16:35:58 -0700

    Add configurable log level

--- src/services/logger.js
+++ src/services/logger.js
@@ -2,9 +2,22 @@
 
 const betterLogging = require('better-logging');
 const { Theme } = betterLogging;
+const config = require('../.config.json');
 
 const fileName = new Date().toLocaleDateString().replace(/\//g, '-');
 betterLogging(console, {
     color: Theme.dark,
     saveToFile: `./logs/${fileName}.log`,
 });
+
+console.logLevel = config.logLevel;
+/**
+ * debug: 4
+ * log: 3
+ * info: 2
+ * warn: 1
+ * error: 0
+ * line: 1
+ * turn off all logging: -1
+ * default: 3
+ */
